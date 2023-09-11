395ec56e1f9e383f1d0e673ec1f1b859a478dbce
Sultan Keles <115416147+sultankeles@users.noreply.github.com>
2023-09-11 21:52:41 +0300

    Add files via upload

---
+++ SQL-1.sql
@@ -0,0 +1,206 @@
+-- SELECT empname FROM employees;
+-- select empname, empsurname from employees;
+
+-- SELECT empname FROM employees;
+-- select empname, empsurname
+-- from employees;
+
+-- SELECT empname FROM employees;
+-- select empname, empsurname
+-- from employees;
+-- select *
+-- from employees;
+
+-- SELECT empname FROM employees;
+-- select empname, empsurname
+-- from employees;
+
+-- SELECT *
+-- FROM employees;
+
+-- SELECT DISTINCT region
+-- FROM employees;
+
+-- SELECT empname FROM employees;
+-- select empname, empsurname
+-- from employees;
+
+-- SELECT *
+-- FROM employees;
+
+-- SELECT DISTINCT region
+-- FROM employees;
+
+-- SELECT DISTINCT gender
+-- FROM employees;
+
+-- SELECT empname FROM employees;
+-- select empname, empsurname
+-- from employees;
+
+-- SELECT *
+-- FROM employees;
+
+-- SELECT DISTINCT region
+-- FROM employees;
+
+-- SELECT DISTINCT gender
+-- FROM employees;
+
+-- SELECT empname FROM employees;
+-- select empname, empsurname
+-- from employees;
+
+-- SELECT *
+-- FROM employees;
+
+-- SELECT DISTINCT region
+-- FROM employees;
+
+-- SELECT DISTINCT gender
+-- FROM employees;
+
+-- SELECT empname FROM employees;
+-- select empname, empsurname
+-- from employees;
+
+-- SELECT *
+-- FROM employees;
+
+-- SELECT DISTINCT region
+-- FROM employees;
+
+-- SELECT DISTINCT gender
+-- FROM employees;
+
+-- SELECT DISTINCT gender, region
+-- FROM employees;
+
+-- SELECT *
+-- FROM employees
+-- WHERE gender = 'Male'; 
+
+-- SELECT empname FROM employees;
+-- select empname, empsurname
+-- from employees;
+
+-- SELECT *
+-- FROM employees;
+
+-- SELECT DISTINCT region
+-- FROM employees;
+
+-- SELECT DISTINCT gender
+-- FROM employees;
+
+-- SELECT DISTINCT gender, region
+-- FROM employees;
+
+-- SELECT *
+-- FROM employees
+-- WHERE gender = 'Male' and job = 'Sales'; 
+
+-- SELECT empname FROM employees;
+-- select empname, empsurname
+-- from employees;
+
+-- SELECT *
+-- FROM employees;
+
+-- SELECT DISTINCT region
+-- FROM employees;
+
+-- SELECT DISTINCT gender
+-- FROM employees;
+
+-- SELECT DISTINCT gender, region
+-- FROM employees;
+
+-- SELECT *
+-- FROM employees
+-- WHERE gender = 'Male' or job = 'Sales'; 
+
+-- SELECT empname FROM employees;
+-- select empname, empsurname
+-- from employees;
+
+-- SELECT *
+-- FROM employees;
+
+-- SELECT DISTINCT region
+-- FROM employees;
+
+-- SELECT DISTINCT gender
+-- FROM employees;
+
+-- SELECT DISTINCT gender, region
+-- FROM employees;
+
+-- SELECT *
+-- FROM employees
+-- WHERE gender = 'Male' or job = 'Sales'; 
+
+-- SELECT *
+-- FROM employees
+-- WHERE salary < 3000 and gender = 'Female' or region = 'Central';
+
+-- SELECT empname FROM employees;
+-- select empname, empsurname
+-- from employees;
+
+-- SELECT *
+-- FROM employees;
+
+-- SELECT DISTINCT region
+-- FROM employees;
+
+-- SELECT DISTINCT gender
+-- FROM employees;
+
+-- SELECT DISTINCT gender, region
+-- FROM employees;
+
+-- SELECT *
+-- FROM employees
+-- WHERE gender = 'Male' or job = 'Sales'; 
+
+-- SELECT *
+-- FROM employees
+-- -- WHERE salary < 3000 and gender = 'Female' or region = 'Central';
+-- WHERE gender = 'Female' and hiredate > '2018-02-16';
+
+-- SELECT empname FROM employees;
+-- select empname, empsurname
+-- from employees;
+
+-- SELECT *
+-- FROM employees;
+
+-- SELECT DISTINCT region
+-- FROM employees;
+
+-- SELECT DISTINCT gender
+-- FROM employees;
+
+-- SELECT DISTINCT gender, region
+-- FROM employees;
+
+-- SELECT *
+-- FROM employees
+-- WHERE gender = 'Male' or job = 'Sales'; 
+
+-- SELECT *
+-- FROM employees
+-- -- WHERE salary < 3000 and gender = 'Female' or region = 'Central';
+-- -- WHERE gender = 'Female' and hiredate > '2018-02-16';
+-- WHERE gender = 'Male' and hiredate <= '2018-05-25' or job = 'Technical';
+
+-- SELECT *
+-- FROM employees
+-- WHERE job = 'Sales';
+
+-- SELECT *
+-- FROM employees
+-- WHERE job = 'Sales' LIMIT 10;
+
+SELECT * FROM employees WHERE job = 'Sales' LIMIT 5;
